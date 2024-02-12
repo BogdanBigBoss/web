2902d9cbc35ac2e10ca5bc6dab1eaaa863248f81
BogdanBigBoss <denisenkob15@gmail.com>
2024-02-12 19:04:21 +0300

    Update test.js

--- test.js
+++ test.js
@@ -12,8 +12,6 @@ console.log(4);
 
 alert("Hello!");
 
-console.log(fetch("./test.json"));
-
-alert(fetch("./test.json"));
+fetch("./test.json").then(response => response.json()).then(json => console.log(json));
 
 
